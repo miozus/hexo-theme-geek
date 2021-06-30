7b21442b3dfa7f3b7f7de4426722872db5e388ca
amazm <lss811@outlook.com>
2021-07-01 02:13:52 +0800

    404.html : 👀 电脑桌面，眼睛跟随鼠标动; back-btn: history.back(); favicon : dynamic change by weekday

--- source/js/js.js
+++ source/js/js.js
@@ -1,17 +1,19 @@
 var web_style = $("#web_style").val();
 var valine_appid = $("#valine_appid").val();
 var valine_appKey = $("#valine_appKey").val();
+// 📅 
+var weekday = new Date().getDay();
 
 new Valine({
-    el: '#vcomments',
+    el: "#vcomments",
     appId: valine_appid,
     appKey: valine_appKey,
-    placeholder: '发一条友善的评论',
-    avatar: 'retro',
-})
+    placeholder: "发一条友善的评论",
+    avatar: "retro",
+});
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.querySelectorAll('pre').forEach((block) => {
+document.addEventListener("DOMContentLoaded", (event) => {
+    document.querySelectorAll("pre").forEach((block) => {
         hljs.highlightBlock(block);
     });
 });
@@ -22,7 +24,7 @@ function setCookie(key, value) {
 
 function getCookie(key) {
     var data = localStorage.getItem(key);
-    return data
+    return data;
 }
 
 function updateStyle() {
@@ -31,34 +33,36 @@ function updateStyle() {
         $(".flink").attr("style", "color: #51525d;");
         $(".ba").attr("style", "color: #51525d;");
         $("#bodyx").attr("class", "bg_while");
-        $("#update_style").attr('checked', false);
+        $("#update_style").attr("checked", false);
     } else {
         $("#footer").attr("style", "");
         $(".flink").attr("style", "");
         $("#bodyx").attr("class", "");
         $(".ba").attr("style", "");
-        $("#update_style").attr('checked', true);
+        $("#update_style").attr("checked", true);
     }
 }
 
 if (getCookie("style") == null) {
-    setCookie("style", web_style)
+    setCookie("style", web_style);
     updateStyle();
 } else if (getCookie("style") == "white") {
-    setCookie("style", "white")
+    setCookie("style", "white");
     updateStyle();
 } else if (getCookie("style") == "black") {
-    setCookie("style", "black")
+    setCookie("style", "black");
     updateStyle();
 }
 
 $("#update_style").change(function() {
-    var style = $("#update_style").is(':checked');
+    var style = $("#update_style").is(":checked");
     if (style) {
-        setCookie("style", "black")
+        setCookie("style", "black");
         updateStyle();
     } else {
-        setCookie("style", "white")
+        setCookie("style", "white");
         updateStyle();
     }
 });
+
+$("#favicon").attr("href", "/img/favicon_" + weekday + ".ico");
